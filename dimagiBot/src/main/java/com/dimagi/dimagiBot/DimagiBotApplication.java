2a3d93a1fce86225dd8ffef76b25543180e7ae3b
Sethi <harshita.sethi@accenture.com>
2022-08-26 15:00:54 +0530

    First commit

---
+++ dimagiBot/src/main/java/com/dimagi/dimagiBot/DimagiBotApplication.java
@@ -0,0 +1,13 @@
+package com.dimagi.dimagiBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimagiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimagiBotApplication.class, args);
+	}
+
+}
---
+++ dimagiBot/src/test/java/com/dimagi/dimagiBot/DimagiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimagi.dimagiBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimagiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
